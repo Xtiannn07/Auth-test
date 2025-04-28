08982add80ff9c080fdb3f2ccaa78fd5bdd91f51
Christian <galinganchristianmarc@gmail.com>
2025-04-28 15:21:29 +0800

    idk

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,36 @@ export default defineConfig({
   ],
   build: {
     target: 'esnext', // This enables support for top-level await
+    chunkSizeWarningLimit: 1000, // Increased from default 500kb to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Group React and related packages
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          
+          // If you're using any state management libraries
+          'state-management': ['redux', 'react-redux', 'recoil', 'zustand'].filter(
+            dep => {
+              try {
+                return !!require.resolve(dep)
+              } catch (e) {
+                return false
+              }
+            }
+          ),
+          
+          // UI libraries if you're using any
+          'ui-libs': ['@mui/material', '@emotion/react', '@emotion/styled', 'antd', 'chakra-ui'].filter(
+            dep => {
+              try {
+                return !!require.resolve(dep)
+              } catch (e) {
+                return false
+              }
+            }
+          )
+        }
+      }
+    }
   }
 })
